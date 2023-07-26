7a285b4e7c044098bffbb3853edd45212ab7046b
mrrased <rashedtechdr.786@gmail.com>
2023-07-26 20:52:21 +0600

    footer added [Done]

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import logo from '@/assets/images/technet-logo-white.png';
 import { RiFacebookBoxFill, RiInstagramLine } from 'react-icons/ri';
+
+const imgLogo =
+  'https://www.clipartmax.com/png/middle/89-891042_letter-b-logo-design-free-b-letter-logo-design-png.png';
+
 export default function Footer() {
   const date = new Date();
   const year = date.getFullYear();
@@ -9,7 +12,7 @@ export default function Footer() {
     <div className="bg-[#242630] text-secondary p-20">
       <div className="flex justify-between">
         <div>
-          <img className="h-10" src={logo} alt="Logo" />
+          <img className="h-10" src={imgLogo} alt="Logo" />
         </div>
         <div className="flex gap-20">
           <ul className="space-y-2">
@@ -34,7 +37,7 @@ export default function Footer() {
       <div className="flex w-full mt-20 gap-5">
         <p>Privacy Policy</p>
         <p>Terms & Condition</p>
-        <p className="ml-auto"> &#169; TechNet {year}</p>
+        <p className="ml-auto"> &#169; Books House {year}</p>
       </div>
     </div>
   );
